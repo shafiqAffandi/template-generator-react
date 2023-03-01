f13bf80075721f815a7b0ae9f083ea36de901161
msa@work <excelnut.cracker@gmail.com>
2023-03-01 12:43:36 +0700

    fix: scrollable modal

--- src/components/ui-components/ModalComponent.tsx
+++ src/components/ui-components/ModalComponent.tsx
@@ -8,7 +8,7 @@ export function Modal({ children, className }: Props) {
     <div className="fixed top-0 left-0 right-0 bottom-0 bg-zinc-900/50 backdrop-blur-sm">
       <div
         className={
-          "fixed top-1/2 left-1/2 z-10 inline-block h-auto w-3/5 -translate-y-1/2 -translate-x-1/2 rounded bg-white p-2 " +
+          "fixed top-1/2 left-1/2 z-10 inline-block max-h-screen w-3/5 -translate-y-1/2 -translate-x-1/2 overflow-y-auto overflow-x-hidden scroll-smooth rounded bg-white p-2 " +
             className ?? ""
         }
       >
